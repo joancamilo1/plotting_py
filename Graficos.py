35401e24e00bc54d180da781f000f5ae3a066db8
joan camilo tamayo <joancamilot@gmail.com>
2023-10-30 11:14:36 -0500

    Actualiza el codigo

--- Graficos.py
+++ Graficos.py
@@ -140,7 +140,7 @@ ax.legend()
 
 plt.show()
 
- 
+# ================================== real ========================= 
 import seaborn as sns
 import pandas as pd
 import plotly.graph_objs as go
@@ -212,5 +212,124 @@ for patch in bar_plot.patches:
 
 # Mostrar la gráfica utilizando Plotly
 pyo.iplot(fig)
+
+
+
+
+
+#  ======================= DEA plot ========================================
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Supongamos que tienes un DataFrame llamado BDL_eficiencia con una columna "Eficiencia"
+# Calcula la eficiencia relativa en tu DataFrame después de realizar el DEA
+
+# Clasifica las unidades por eficiencia de mayor a menor
+BDL_eficiencia = BDL_eficiencia.sort_values(by="Eficiencia_ENTRADA", ascending=False)
+
+# Crea un gráfico de dispersión con los números de unidad en el eje x y la eficiencia en el eje y
+plt.figure(figsize=(10, 6))
+plt.scatter(range(1, len(BDL_eficiencia) + 1), BDL_eficiencia["Eficiencia_ENTRADA"], label="Eficiencia_ENTRADA")
+
+# Dibuja una línea horizontal en la "frontera eficiente"
+plt.axhline(y=BDL_eficiencia.iloc[0]["Eficiencia_ENTRADA"], color="red", linestyle="--", label="Frontera Eficiente")
+
+plt.xlabel("Número de Unidad")
+plt.ylabel("Eficiencia")
+plt.title("Gráfico de la Frontera Eficiente")
+plt.legend()
+plt.show()
+
+
+
+import seaborn as sns
+import pandas as pd
+import plotly.express as px
+
+# Supongamos que tienes la tabla BDL_eficiencia
+
+# Filtra las observaciones eficientes (puedes usar tu propio criterio de eficiencia)
+observaciones_eficientes = BDL_eficiencia[BDL_eficiencia['Eficiencia_ENTRADA'] == 1]
+
+# Crea un gráfico de dispersión con Plotly Express
+fig = px.scatter(BDL_eficiencia, x='Costo_Medicamentos', y='Egresos', title='Frontera Eficiente')
+
+# Personaliza el gráfico
+fig.update_traces(marker=dict(size=8, color='blue', opacity=0.5), selector=dict(mode='markers'), name='Ineficiente')
+
+# Agrega las observaciones eficientes al gráfico
+fig.add_trace(go.Scatter(
+    x=observaciones_eficientes['Costo_Medicamentos'],
+    y=observaciones_eficientes['Egresos'],
+    mode='markers',
+    marker=dict(size=8, color='red', opacity=0.7),
+    name='Eficiente'
+))
+
+# Muestra el gráfico
+fig.show()
+
+
+
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Supongamos que tienes la tabla BDL_eficiencia
+
+# Filtra las observaciones eficientes (puedes usar tu propio criterio de eficiencia)
+observaciones_eficientes = BDL_eficiencia[BDL_eficiencia['Eficiencia_ENTRADA'] == 1]
+
+# Crea un gráfico de dispersión con Matplotlib
+plt.figure(figsize=(10, 6))
+
+# Grafica las observaciones ineficientes
+plt.scatter(BDL_eficiencia[BDL_eficiencia['Eficiencia_ENTRADA'] != 1]['Costo_Medicamentos'],
+            BDL_eficiencia[BDL_eficiencia['Eficiencia_ENTRADA'] != 1]['Egresos'],
+            c='blue', label='Ineficiente', alpha=0.5)
+
+# Grafica las observaciones eficientes
+plt.scatter(observaciones_eficientes['Costo_Medicamentos'],
+            observaciones_eficientes['Egresos'],
+            c='red', label='Eficiente', alpha=0.7)
+
+# Personaliza el gráfico
+plt.title('Frontera Eficiente')
+plt.xlabel('Costo de Medicamentos')
+plt.ylabel('Egresos')
+plt.legend()
+plt.grid(True)
+
+# Muestra el gráfico
+plt.show()
+
+
+import plotly.express as px
+
+# Supongamos que tienes la tabla BDL_eficiencia
+
+# Filtra las observaciones eficientes (puedes usar tu propio criterio de eficiencia)
+observaciones_eficientes = BDL_eficiencia[BDL_eficiencia['Eficiencia'] == 1]
+
+# Crea un gráfico de dispersión interactivo con Plotly Express
+fig = px.scatter(observaciones_eficientes, x='Costo_Medicamentos', y='Egresos',
+                 title='Frontera Eficiente', color_discrete_sequence=['red'])
+
+# Personaliza el gráfico
+fig.update_layout(
+    xaxis_title='Costo de Medicamentos',
+    yaxis_title='Egresos'
+)
+
+# Muestra el gráfico interactivo
+fig.show()
+
+
+
+
+
+
+
 
 
